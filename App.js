6a6fc672e9e943d65584ff41e6d95510a2a2f4f4
Alok <ajalokgupta@gmail.com>
2023-07-26 12:45:19 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,11 @@
+
+    const parent = React.createElement('div', {id:'parent'} ,[React.createElement('div', {id:'child'},[React.createElement('h1', {}, 'here is an h1 tag'),React.createElement('h2', {}, 'here is an h2 tag')]),
+    React.createElement('div', {id:'child'},[React.createElement('h1', {}, 'here is an h1 tag'),React.createElement('h2', {}, 'here is an h2 tag')])]
+    
+    )
+
+    console.log(parent)
+
+    const root = ReactDOM.createRoot(document.getElementById('root'))
+
+    root.render(parent);
